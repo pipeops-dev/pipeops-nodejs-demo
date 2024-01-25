a008cd51cb1c3c5f534d127c87ee4ea8d8cd4122
Ewere Diagboya <nimboya@gmail.com>
2024-01-25 06:29:45 +0100

    update pipeops default message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Welcome to Pipeops NodeJS Deployment!');
 });
 
 app.listen(port, () => {
